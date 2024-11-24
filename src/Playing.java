75276d9ddb560ae336f46bfd83fa3d290e524a70
sara najati <saranajatilaptop@gmail.com>
2024-11-24 12:52:21 +0300

    dfs recersion quiz

--- src/Playing.java
+++ src/Playing.java
@@ -44,22 +44,30 @@ public class Playing {
             Graph graph = new Graph();
             graph.dfs(level, initState);
       }
+      void dfs(int level) {
+            State initState = new State();
+            initState.level(level);
+            initState.dimension(level);
+            Graph graph = new Graph();
+            graph.dfsRecersion(level, initState);
+      }
 
       public static void main(String[] args) {
             Playing play = new Playing();
-            Scanner input2 = new Scanner(System.in);
-            System.out.println("Do you want to play (p), or get the solution by BFS(b) or DFS(d)");
-            String game = input2.nextLine();
-            Scanner input1 = new Scanner(System.in);
-            System.out.println("Choose a level from 1 to 3");
-            int level = input1.nextInt();
-            if ("p".equals(game))
-                  play.startPlaying(level);
-            if ("b".equals(game))
-                  play.bfsSolving(level);
-            if ("d".equals(game))
-                  play.dfsSolving(level);
-            input1.close();
-            input2.close();
+            play.dfs(3);
+            // Scanner input2 = new Scanner(System.in);
+            // System.out.println("Do you want to play (p), or get the solution by BFS(b) or DFS(d)");
+            // String game = input2.nextLine();
+            // Scanner input1 = new Scanner(System.in);
+            // System.out.println("Choose a level from 1 to 3");
+            // int level = input1.nextInt();
+            // if ("p".equals(game))
+            //       play.startPlaying(level);
+            // if ("b".equals(game))
+            //       play.bfsSolving(level);
+            // if ("d".equals(game))
+            //       play.dfsSolving(level);
+            // input1.close();
+            // input2.close();
       }
 }
